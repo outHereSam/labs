b26ec970aa3c747253a349a20b08efc86bb100a9
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-06-11 15:50:17 +0000

    Implements string reverse function

--- lab1.js
+++ lab1.js
@@ -1,7 +1,7 @@
 // Lab Activity: Functional Data Transformation Playground
 
 // String Transformations
-// a. Capitalize
+// a. Capitalize String
 function capitalize(str) {
   let capitalized = str[0].toUpperCase();
   for (let i = 1; i < str.length; i++) {
@@ -11,4 +11,16 @@ function capitalize(str) {
   return capitalized;
 }
 
-console.log(capitalize("cat"));
+// console.log(capitalize("cat"));
+
+// b. Reverse String
+function reverse(str) {
+  strArr = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    strArr += str[i];
+  }
+  return strArr;
+}
+
+console.log(reverse("cat"));
