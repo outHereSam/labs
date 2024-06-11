5f5d3dd17b5e7a1036c8d6adc8ab8aa425230bdc
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-06-11 16:08:53 +0000

    Implements the sum function

--- lab1.js
+++ lab1.js
@@ -86,3 +86,18 @@ function filterEven(arr) {
 }
 
 // console.log(filterEven([1, 2, 3, 4]));
+
+/*----------------------------------------------
+ c. Sum
+-------------------------------------------------*/
+function sum(arr) {
+  let total = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    total += arr[i];
+  }
+
+  return total;
+}
+
+console.log(sum([1, 2, 3, 4]));
