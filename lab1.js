d40cd34e218bd68da773138280c1ad73e99bc7ed
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-06-11 16:10:28 +0000

    Implements average function

--- lab1.js
+++ lab1.js
@@ -100,4 +100,19 @@ function sum(arr) {
   return total;
 }
 
-console.log(sum([1, 2, 3, 4]));
+// console.log(sum([1, 2, 3, 4]));
+
+/*----------------------------------------------
+ c. Average
+-------------------------------------------------*/
+function average(arr) {
+  let total = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    total += arr[i];
+  }
+
+  return total / arr.length;
+}
+
+console.log(average([1, 2, 3]));
